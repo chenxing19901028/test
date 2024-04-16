ac0582c96f95960537fab0bc9308a6abe43790b1
NT0009 <admin@163.com>
2024-04-16 16:57:19 +0800

    222

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        // 中国人不骗中国人111
+        // 中国人不骗中国人123
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
